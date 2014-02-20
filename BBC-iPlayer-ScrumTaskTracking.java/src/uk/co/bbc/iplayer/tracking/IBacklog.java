25cfbe41a46fc507b6d7ee0accbc6149d94d6b8b
desilva <desilva@cs.umn.edu>
2014-02-20 13:36:50 -0600

    desilvai - TaskTracking.java - Added LICENSE file draft.

---
+++ BBC-iPlayer-ScrumTaskTracking.java/src/uk/co/bbc/iplayer/tracking/IBacklog.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright (C) 2014, Ian J. De Silva
+ * All rights reserved.
+ * 
+ * Use, distribution, and modification of this work for any purpose is strictly
+ * prohibited without the express consent of the copyright holder except as 
+ * permitted by law.
+ */
+package uk.co.bbc.iplayer.tracking;
+
+/**
+ * @author desilva
+ *
+ */
+public interface IBacklog
+{
+
+}
